c04af2f83ea5b9c29aceefd034221347e9e4a75c
lyctdthyr <yiyang.songyy@gmail.com>
2026-09-08 18:22:34 +0000

    sfvvfdfv

---
+++ crash-course.py
@@ -0,0 +1,37 @@
+# wow i can print sht
+
+print('Hello, world!')
+
+# i am the declarer of variables
+
+a = 4 # this... is an integer, boy
+b = 6.7 # this... is a float, boy
+c = 'lkasnkj' # this... is a string, boy
+d = False # this... is a boolean, boy
+e = 'csaea'
+
+print(a, b, c, d, e)
+
+# these are some epic operations:
+# + - * /
+# % <-- modulus
+# ** <-- exponent
+
+# compound functions are:
+# += -= /=
+
+f = 196 ** 23 % 11 ** 4
+print(f)
+f += 7
+print(f)
+
+# aight now we're doin fstrings or something
+# yk'now
+# happens in a print or whatever
+
+print(f'{f} is equal to {f}')
+
+# python runs code from top to bottom
+
+f = 23498024
+print(f'{f} is such a cool number')
