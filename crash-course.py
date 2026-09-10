b1505a7629285da2810035af51c8b76dd7947803
lyctdthyr <yiyang.songyy@gmail.com>
2026-09-10 16:27:18 +0000

    dfvfvdsc fvds

--- crash-course.py
+++ crash-course.py
@@ -25,7 +25,7 @@ print(f)
 f += 7
 print(f)
 
-# aight now we're doin fstrings or something
+# aight now we're doin f-strings or something
 # yk'now
 # happens in a print or whatever
 
@@ -35,3 +35,73 @@ print(f'{f} is equal to {f}')
 
 f = 23498024
 print(f'{f} is such a cool number')
+
+# uhh more booleans time i suppose
+# True or False type shi
+# < > <= >= == !=
+
+print(4 < 5)
+print(7 == 4, 7 != 2, 33 > 8, 4 < 2.5)
+
+fdnjlnjldf = 5 == 6.78
+print (fdnjlnjldf)
+
+uu = 'skwww' == 'skwww'
+print(uu)
+
+pp = 'haha' == 'Haha'
+print(pp)
+
+# logical operatorssssssssssssssssssssssssssssssssssssssssssssssssssssss
+# this keyboard is so cool
+# in order of precedence
+# not  and  or
+
+pppp = False
+nopp = True
+
+print(not pppp)
+print(pppp and nopp)
+print(pppp or nopp)
+print(pppp or nopp and not pppp)
+print(not nopp)
+
+print('-separatingTextHere-')
+
+print(pppp and pppp)
+print(pppp and nopp)
+print(nopp and pppp)
+print(nopp and nopp)
+
+print(pppp or pppp)
+print(pppp or nopp)
+print(nopp or pppp)
+print(nopp or nopp)
+
+# i cried at the end of the movie when castius said "it's castin time" and casted all over
+
+g = int(5.999999999999999)
+print(g)
+
+h = int(b)
+print(h)
+
+# i wept at the conclusion of the film when stringius uttered "it is stringin time" and strung all around
+
+s1 = 'gn'
+s2 = ' and '
+s3 = 'goodbye'
+end = s1 + s2 + s3 # concatenation or sum
+end += ", twin"
+print(end)
+print(end + '\n')
+print('balls' + '\n')
+
+# i bawled at the closing of the motion picture when mathlibrarius declared "it is now mathlibrarin time" and mathlibraried all across
+# min, max, n sqrt
+
+import math
+print(math.sqrt(67))
+print(math.ceil(41.34))
+print(math.floor(17.69))
+print(math.pow(4, 48))
